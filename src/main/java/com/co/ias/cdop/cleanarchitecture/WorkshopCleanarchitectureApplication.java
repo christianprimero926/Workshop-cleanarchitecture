7a6ad853f4000a797a2ad6b579bfcc47713cf276
Christian David Ospina Primero <christianprimero26@hotmail.com>
2024-10-05 19:32:11 -0500

    initial commit

---
+++ src/main/java/com/co/ias/cdop/cleanarchitecture/WorkshopCleanarchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.co.ias.cdop.cleanarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopCleanarchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopCleanarchitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/ias/cdop/cleanarchitecture/WorkshopCleanarchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.ias.cdop.cleanarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopCleanarchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
